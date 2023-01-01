6fe0daf33bfad01cc5e10227baf0639fae581223
John Fieweger <john.fieweger@io-group.com>
2023-01-01 21:33:01 +0100

    add Accordions with Items with Checkboxes bug fixes

--- Screens/ReceptScreen/Ingredients.js
+++ Screens/ReceptScreen/Ingredients.js
@@ -1,11 +1,78 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { List, Checkbox } from 'react-native-paper';
+
+const sections = [
+    {
+        title: "Fleisch",
+        items: [
+            "Huhn",
+            "Rind"
+        ]
+    },
+    {
+        title: "Fisch",
+        items: [
+            "Lachs",
+            "Sardine"
+        ]
+    },
+    {
+        title: "Gemüse",
+        items: [
+            "Tomate",
+            "Knoblauch"
+        ]
+    },
+
+]
 
 export default function IngredientsScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Zutaten screen</Text>
-
-        </View>
+        <View style={styles.container}>
+            <View style={styles.ingredContainer}>
+                <List.Section >
+                    <List.Accordion
+                        title="Abschnitt ..."
+                        left={props => <List.Icon {...props}
+                        />}>
+                        {sections.map((item, index) =>
+                            <List.Accordion
+                                style={styles.pLeft}
+                                title={item.title}
+                                left={props => <List.Icon {...props}
+                                />}>
+                                {item.items.map((item, index) =>
+                                    <List.Item
+                                        title={item}
+                                        left={() => (
+                                            <Checkbox
+                                                status='unchecked'
+                                            />
+                                        )} />
+                                )}
+                            </List.Accordion>
+                        )}
+                    </List.Accordion>
+                </List.Section>
+            </View>
+            <View >
+                <Text>Zutaten screen</Text>
+            </View>
+        </View >
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row'
+    },
+    ingredContainer: {
+        width: '60%',
+        // backgroundColor: 'green'
+    },
+    pLeft: {
+        marginLeft: 32
+    }
+})
